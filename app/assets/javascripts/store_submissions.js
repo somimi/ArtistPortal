6c63178cfd66cf4a93f11ffcc16a30b046a00b02
Raymond Forbes <raymond.forbes@gmail.com>
2012-01-21 16:15:41 -0800

    added store stuff

--- app/assets/javascripts/store_submissions.js
+++ app/assets/javascripts/store_submissions.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	
 	$("#store-type-other").hide();
+	$("#presentation-other").hide();
 	
 	$("#store_submission_type").change(function(){
 		var val = $(this).val();
@@ -14,6 +15,23 @@ $(document).ready(function(){
 		else
 		{
 			$("#store-type-other").hide("slow");
+		}
+
+		 
+	});
+	
+	$("#store_submission_presentation").change(function(){
+		var val = $(this).val();
+		
+		
+		
+		if (val == "Other")
+		{
+			$("#presentation-other").show("slow");
+		}
+		else
+		{
+			$("#presentation-other").hide("slow");
 		}
 
 		 

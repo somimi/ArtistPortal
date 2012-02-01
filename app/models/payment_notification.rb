054fffa9d9ab73c03d5d61f6dc0a25f449bfdcfb
Raymond Forbes <raymond.forbes@gmail.com>
2012-02-01 12:06:20 -0800

    fix for payment processing

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -8,13 +8,13 @@ class PaymentNotification < ActiveRecord::Base
   
   def payment_accepted
     if status == "Completed"
-      if product == 1
+      if product == "1"
         user.artist.update_attribute(:visual_paid, true)
-      elsif product == 2
+      elsif product == "2"
         user.artist.update_attribute(:literary_paid, true)
-      elsif product == 3
+      elsif product == "3"
         user.artist.update_attribute(:installation_paid, true)
-      else product == 4
+      else product == "4"
         user.artist.update_attribute(:film_paid, true)
       end
     end

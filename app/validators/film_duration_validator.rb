67cab3f17dff6f68375261b6e1f7a9965b5b0109
Raymond Forbes <raymond.forbes@gmail.com>
2012-02-03 01:54:57 -0800

    stuff

--- app/validators/film_duration_validator.rb
+++
@@ -1,7 +0,0 @@
-class FilmDurationValidator < ActiveModel::EachValidator  
-  def validate_each(object, attribute, value)  
-    unless value > 30  
-      object.errors[attribute] << (options[:message] || "must be under 30 minutes")  
-    end  
-  end  
-end

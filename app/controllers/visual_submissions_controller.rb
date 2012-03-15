66cca85bf06a54d597a256e5524c7555c7f72fb4
Raymond Forbes <raymond.forbes@gmail.com>
2012-03-15 11:33:15 -0700

    fixed count

--- app/controllers/visual_submissions_controller.rb
+++ app/controllers/visual_submissions_controller.rb
@@ -16,15 +16,15 @@ class VisualSubmissionsController < ApplicationController
       if params[:filter] == "voted"
         @visual_submissions = VisualSubmission.voted(current_user.juror)
         session[:query] = @visual_submissions.map(&:id)
-        @count = @visual_submission.size
+        @count = @visual_submissions.count
       elsif params[:filter] == "not_voted"
         @visual_submissions = VisualSubmission.not_voted(current_user.juror)
         session[:query] = @visual_submissions.map(&:id)
-        @count = @visual_submission.size
+        @count = @visual_submissions.count
       else
         @visual_submissions = VisualSubmission.juror_all
         session[:query] = @visual_submissions.map(&:id)
-        @count = @visual_submission.size
+        @count = @visual_submissions.count
       end
     else
       @visual_submissions = VisualSubmission.search(params[:search]).order(sort_column + ' ' + sort_direction).page(params[:page]).per(30)

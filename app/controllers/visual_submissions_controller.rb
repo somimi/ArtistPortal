a2880c2e47abef54c457cf79ae60ac03e8e36608
Raymond Forbes <raymond.forbes@gmail.com>
2012-03-17 11:37:01 -0700

    stuff

--- app/controllers/visual_submissions_controller.rb
+++ app/controllers/visual_submissions_controller.rb
@@ -136,6 +136,6 @@ class VisualSubmissionsController < ApplicationController
   end  
     
   def sort_direction  
-    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"    
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "desc"    
   end
 end

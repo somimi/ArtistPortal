b4f90e5add9a3d3bb89d508b1dedce1bde92226d
Raymond Forbes <raymond.forbes@gmail.com>
2012-04-18 22:29:44 -0700

    more fixes

--- app/controllers/visual_submissions_controller.rb
+++ app/controllers/visual_submissions_controller.rb
@@ -9,7 +9,9 @@ class VisualSubmissionsController < ApplicationController
     if current_user.is_artist? || current_user.is_invited?
       @visual_submissions = current_user.artist.visual_submissions
     elsif current_user.is_admin? || current_user.is_handler?
-      @visual_submissions = VisualSubmission.search(params[:search]).order(sort_column + ' ' + sort_direction).page(params[:page]).per(30)
+      #@visual_submissions = VisualSubmission.search(params[:search]).order(sort_column + ' ' + sort_direction).page(params[:page]).per(30)
+      @visual_submissions = VisualSubmission.all
+      
       session[:query] = @visual_submissions.map(&:id)
       VisualSubmission.search(params[:search]).count
     elsif current_user.is_art_guide?

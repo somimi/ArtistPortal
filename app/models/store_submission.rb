cc5c3b4464db764d01fd0e411b35699ac6b280cc
Raymond Forbes <raymond.forbes@gmail.com>
2012-06-25 00:56:58 -0700

    more for shipping store

--- app/models/store_submission.rb
+++ app/models/store_submission.rb
@@ -11,7 +11,7 @@ class StoreSubmission < ActiveRecord::Base
                     :path => ':rails_root/store-secure/system/:attachment/:id/:style/:basename.:extension',
                     :url => '/:class/:id/:attachment?style=:style'
   
-  attr_accessible :artist_id, :title, :description, :quantity, :quantity_accepted, :accepted, :image, :type, :type_other, :price, :height, :width, :depth, :h_unit, :w_unit, :d_unit, :presentation, :presentation_other, :provide_display, :notes, :dimension, :visual_submission_id, :admin_notes, :quantity_received, :quality, :quality_notes, :display_included, :display_notes, :packing_material, :packing_material_notes, :received
+  attr_accessible :artist_id, :title, :description, :quantity, :quantity_accepted, :accepted, :image, :type, :type_other, :price, :height, :width, :depth, :h_unit, :w_unit, :d_unit, :presentation, :presentation_other, :provide_display, :notes, :dimension, :visual_submission_id, :admin_notes, :quantity_received, :quality, :quality_notes, :display_included, :display_notes, :packing_material, :packing_material_notes, :received, :quantity_returned, :quantity_sold
   
   def self.search(search)        
     if search
@@ -19,5 +19,12 @@ class StoreSubmission < ActiveRecord::Base
     else  
       scoped  
     end  
+  end
+  
+  def discrepancy  
+    if !quantity_returned.nil? and !quantity_sold.nil? and !quantity_received.nil?
+      difference = quantity_recieved - (quantity_returned + quantity_sold) 
+      return difference
+    end
   end
 end

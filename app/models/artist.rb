ad2d0f5f4821f67e56323411a2b73397d86fce74
Aja Hammerly <aja.hammerly+github@gmail.com>
2012-12-31 16:06:02 -0800

    Remove trailing whitespace.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,7 +8,7 @@ class Artist < ActiveRecord::Base
   has_many :performance_submissions
   has_many :dj_submissions
   belongs_to :user
-  
+
   validates_presence_of :first_name, :last_name, :email, :phone, :mailing_address_line1, :mailing_city, :mailing_state, :mailing_country, :bio, :email_confirmation, :mailing_zip
   validates_confirmation_of :email
   attr_accessible :first_name, :last_name, :user_id, :professional_name, :email, :phone, :mailing_address_line1, :mailing_address_line2, :mailing_city, :mailing_zip, :shipping_address_line1, :shipping_address_line2, :shipping_city, :shipping_zip, :shipping_state, :mailing_state, :shipping_country, :mailing_country, :shipping_same_as_mail, :shipping_account, :bio, :require_representation, :literary_paid, :visual_paid, :film_paid, :installation_paid, :shipping_company, :store_paid, :email_confirmation, :delivery_arrangements, :return_arrangements, :representname, :represent_gallery, :acceptance_status, :admin_notes, :shipping_label, :paperwork_complete, :paperwork_notes, :store_shipped_with_with_visual, :pickedup_by, :tracking_number
@@ -18,12 +18,12 @@ class Artist < ActiveRecord::Base
     mailing_country == "Mexico" or mailing_country == "Canada" or mailing_country == "United States"
   end
 
-  def self.search(search)  
-    if search  
+  def self.search(search)
+    if search
       where('first_name LIKE ? OR last_name LIKE ? or email LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%")
-    else  
-      scoped  
-    end  
+    else
+      scoped
+    end
   end
 
 end
--- app/models/film_submission.rb
+++ app/models/film_submission.rb
@@ -1,14 +1,14 @@
 class FilmSubmission < ActiveRecord::Base
-  
+
   belongs_to :artist
-  
+
   validates_presence_of :title, :duration
-  
-  def self.search(search)     
+
+  def self.search(search)
     if search
       joins(:artist).where('title LIKE ? OR first_name LIKE ?', "%#{search}%", "%#{search}%")
-    else  
-      scoped  
-    end  
+    else
+      scoped
+    end
   end
 end

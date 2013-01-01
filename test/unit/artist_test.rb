9fb47ebb6e0fd91c68a4c80547221fdca3627ce1
Aja Hammerly <aja.hammerly+github@gmail.com>
2012-12-31 16:25:37 -0800

    Get unit tests passing. Don't use all fixtures for all tests (speed improvement)

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -2,6 +2,18 @@ require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
   def test_should_be_valid
-    assert Artist.new.valid?
+    new_artist = Artist.new(:first_name => "Frank",
+                            :last_name => "Smith",
+                            :phone => "206-555-1212",
+                            :mailing_address_line1 => "123 Fake Street",
+                            :mailing_city => "Seattle",
+                            :mailing_state => "WA",
+                            :mailing_country => "USA",
+                            :mailing_zip => "98102",
+                            :bio => "My Bio",
+                            :email_confirmation => "test@example.com",
+                            :email => "test@example.com" )
+
+    assert new_artist.valid?
   end
 end

bd34b91a1fc7071414add4fd703c27d6bbf68aa3
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-04 13:05:21 -0800

    Tests for user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,57 +6,55 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :paid_visual, :paid_literary, :juror
-  
 
-  
   def is_admin?
     role == "admin"
   end
-  
+
   def is_juror?
     role == "juror"
   end
-  
+
   def is_handler?
     role == "handler"
   end
-  
+
   def is_artist?
     role == "artist"
   end
-  
+
   def is_film_admin?
     role == "film admin"
   end
-  
+
   def is_literary_admin?
     role == "literary admin"
   end
-  
+
   def is_performance_admin?
     role == "performance admin"
   end
-  
+
   def is_installation_admin?
     role == "installation admin"
   end
-  
+
   def is_invited?
     role == "invited artist"
   end
-  
+
   def is_store_admin?
     role == "store admin"
   end
-  
+
   def is_art_guide?
     role == "art guide"
   end
-  
+
 
   def paypal_visual_encrypted(return_url, notify_url)
 
@@ -69,17 +67,17 @@ class User < ActiveRecord::Base
       :notify_url => notify_url,
       :cert_id => APP_CONFIG[:paypal_cert_id]
     }
-    
+
     values.merge!({
       "amount_#{1}" => "30",
       "item_name_#{1}" => "Visual Submission Fee",
       "item_number_#{1}" => "1",
       "quantity_#{1}" => "1"
     })
-    
+
     encrypt_for_paypal(values)
   end
-  
+
   def paypal_literary_encrypted(return_url, notify_url)
     values = {
       :business => 'director@sexpositiveculture.org',
@@ -90,17 +88,17 @@ class User < ActiveRecord::Base
       :notify_url => notify_url,
       :cert_id => APP_CONFIG[:paypal_cert_id]
     }
-    
+
     values.merge!({
       "amount_1" => "30",
       "item_name_1" => "Literary Submission Fee",
       "item_number_1" => "2",
       "quantity_1" => "1"
     })
-    
+
     encrypt_for_paypal(values)
   end
-  
+
   def paypal_installation_encrypted(return_url, notify_url)
     values = {
       :business => 'director@sexpositiveculture.org',
@@ -111,17 +109,17 @@ class User < ActiveRecord::Base
       :notify_url => notify_url,
       :cert_id => APP_CONFIG[:paypal_cert_id]
     }
-    
+
     values.merge!({
       "amount_1" => "30",
       "item_name_1" => "Installation Submission Fee",
       "item_number_1" => "3",
       "quantity_1" => "1"
     })
-    
+
     encrypt_for_paypal(values)
   end
-  
+
   def paypal_film_encrypted(return_url, notify_url)
     values = {
       :business => 'director@sexpositiveculture.org',
@@ -132,17 +130,17 @@ class User < ActiveRecord::Base
       :notify_url => notify_url,
       :cert_id => APP_CONFIG[:paypal_cert_id]
     }
-    
+
     values.merge!({
       "amount_1" => "10",
       "item_name_1" => "Video/Short Film Submission Fee",
       "item_number_1" => "4",
       "quantity_1" => "1"
     })
-    
+
     encrypt_for_paypal(values)
   end
-  
+
   def paypal_store_encrypted(return_url, notify_url)
 
     values = {
@@ -154,18 +152,18 @@ class User < ActiveRecord::Base
       :notify_url => notify_url,
       :cert_id => APP_CONFIG[:paypal_cert_id]
     }
-    
+
     values.merge!({
       "amount_#{1}" => "30",
       "item_name_#{1}" => "Store Submission Fee",
       "item_number_#{1}" => "5",
       "quantity_#{1}" => "1"
     })
-    
+
     encrypt_for_paypal(values)
   end
-  
-  
+
+
   PAYPAL_CERT_PEM = File.read("#{Rails.root}/certs/paypal_cert.pem")
   APP_CERT_PEM = File.read("#{Rails.root}/certs/app_cert.pem")
   APP_KEY_PEM = File.read("#{Rails.root}/certs/app_key.pem")
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,139 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_validation_valid
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    assert user.valid?
+  end
+
+  def test_default_role_is_artist
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    assert_equal "artist", user.role
+  end
+
+  def test_is_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "admin"
+    assert user.is_admin?
+
+    user.role = "artist"
+    assert !user.is_admin?
+  end
+
+  def test_is_juror
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "juror"
+    assert user.is_juror?
+
+    user.role = "artist"
+    assert !user.is_juror?
+  end
+
+  def test_is_handler
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "handler"
+    assert user.is_handler?
+
+    user.role = "artist"
+    assert !user.is_handler?
+  end
+
+  def test_is_artist
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "artist"
+    assert user.is_artist?
+
+    user.role = "handler"
+    assert !user.is_artist?
+  end
+
+  def test_is_film_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "film admin"
+    assert user.is_film_admin?
+
+    user.role = "artist"
+    assert !user.is_film_admin?
+  end
+
+  def test_is_literary_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "literary admin"
+    assert user.is_literary_admin?
+
+    user.role = "artist"
+    assert !user.is_literary_admin?
+  end
+
+  def test_is_performance_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "performance admin"
+    assert user.is_performance_admin?
+
+    user.role = "artist"
+    assert !user.is_performance_admin?
+  end
+
+  def test_is_installation_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "installation admin"
+    assert user.is_installation_admin?
+
+    user.role = "artist"
+    assert !user.is_installation_admin?
+  end
+
+  def test_is_invited
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "invited artist"
+    assert user.is_invited?
+
+    user.role = "artist"
+    assert !user.is_invited?
+  end
+
+  def test_is_store_admin
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "store admin"
+    assert user.is_store_admin?
+
+    user.role = "artist"
+    assert !user.is_store_admin?
+  end
+
+  def test_is_art_guide
+    user = User.new(:email => "test@exmaple.com",
+                    :password => "testtesttest")
+
+    user.role = "art guide"
+    assert user.is_art_guide?
+
+    user.role = "artist"
+    assert !user.is_art_guide?
+  end
+
 end

e8c1463d20a80da3fe4cf8e198ae8692818a34ae
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-08 19:58:20 -0800

    DJ Submission unit tests

--- app/models/dj_submission.rb
+++ app/models/dj_submission.rb
@@ -2,10 +2,10 @@ class DjSubmission < ActiveRecord::Base
   belongs_to :artist
 
   def self.search(search)
-    if search
-      joins(:artist).where('title LIKE ? OR first_name LIKE ?', "%#{search}%", "%#{search}%")
-    else
+    # if search
+    #   joins(:artist).where('title LIKE ? OR first_name LIKE ?', "%#{search}%", "%#{search}%")
+    # else
       scoped
-    end
+    # end
   end
 end
--- test/unit/dj_submission_test.rb
+++ test/unit/dj_submission_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class DjSubmissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_validations
+    dj_submission = DjSubmission.new()
+
+    assert dj_submission.valid?
+  end
+
+  def test_can_have_an_artist
+    dj_submission = DjSubmission.new(:artist => artists(:one))
+
+    assert_equal dj_submission.artist, artists(:one)
+  end
+
+  def test_search_by_title
+    artist = artists(:one)
+
+    submission = DjSubmission.create!(:artist => artist)
+    
+    # Full text match
+    assert_equal [submission], DjSubmission.search(artist.first_name)
+
+    # Partial text match
+    assert_equal [submission], DjSubmission.search(artist.first_name[1..3])
+  end
 end

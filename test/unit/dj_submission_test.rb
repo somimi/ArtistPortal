5091252e65f1b37ff2c0c34e473607921d08a417
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-08 20:33:21 -0800

    Fix djsubmission search

--- test/unit/dj_submission_test.rb
+++ test/unit/dj_submission_test.rb
@@ -14,10 +14,20 @@ class DjSubmissionTest < ActiveSupport::TestCase
   end
 
   def test_search_by_title
-    artist = artists(:one)
+    artist = Artist.create!(:first_name => "NewArtist",
+                            :last_name => "LastName",
+                            :email => "test@example.com",
+                            :email_confirmation => "test@example.com",
+                            :mailing_address_line1 => "123 Fake Street",
+                            :mailing_city => "Seattle",
+                            :mailing_state => "WA",
+                            :mailing_country => "USA",
+                            :mailing_zip => "98102",
+                            :phone => "123-456-7890",
+                            :bio => "My bio")
 
     submission = DjSubmission.create!(:artist => artist)
-    
+
     # Full text match
     assert_equal [submission], DjSubmission.search(artist.first_name)
 

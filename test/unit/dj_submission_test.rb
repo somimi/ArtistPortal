979c56042603952a3ab65049638536f7f19c50cd
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-01-08 21:01:50 -0800

    Improve DjSubmission tests and add FilmSubmission tests

--- test/unit/dj_submission_test.rb
+++ test/unit/dj_submission_test.rb
@@ -13,18 +13,18 @@ class DjSubmissionTest < ActiveSupport::TestCase
     assert_equal dj_submission.artist, artists(:one)
   end
 
-  def test_search_by_title
-    artist = Artist.create!(:first_name => "NewArtist",
-                            :last_name => "LastName",
-                            :email => "test@example.com",
-                            :email_confirmation => "test@example.com",
+  def test_search_by_artist_first_name
+    artist = Artist.create!(:first_name            => "NewArtist",
+                            :last_name             => "LastName",
+                            :email                 => "test@example.com",
+                            :email_confirmation    => "test@example.com",
                             :mailing_address_line1 => "123 Fake Street",
-                            :mailing_city => "Seattle",
-                            :mailing_state => "WA",
-                            :mailing_country => "USA",
-                            :mailing_zip => "98102",
-                            :phone => "123-456-7890",
-                            :bio => "My bio")
+                            :mailing_city          => "Seattle",
+                            :mailing_state         => "WA",
+                            :mailing_country       => "USA",
+                            :mailing_zip           => "98102",
+                            :phone                 => "123-456-7890",
+                            :bio                   => "My bio")
 
     submission = DjSubmission.create!(:artist => artist)
 
@@ -33,5 +33,9 @@ class DjSubmissionTest < ActiveSupport::TestCase
 
     # Partial text match
     assert_equal [submission], DjSubmission.search(artist.first_name[1..3])
+  end
+
+  def test_search_no_params
+    assert_equal DjSubmission.all.sort, DjSubmission.search(nil).sort
   end
 end
